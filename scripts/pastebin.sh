8033e6665e2ad9358866a606f7143207aa9d57f2
Felix Lehoux <flehoux@gosecure.ca>
2018-02-19 19:06:18 +0000

    Update HTTP to HTTPS, because the api will block all API calls over HTTP.

--- scripts/pastebin.sh
+++ scripts/pastebin.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$(dirname "$0")" ; pwd -P )
 RECENT_PASTES=`mktemp`
 ONION_URLS=`mktemp`
 PURIFIED=`mktemp`
-http_proxy='' curl http://pastebin.com/api_scraping.php | grep -E -o 'http://pastebin\.com/api_scrape_item\.php\?i=[a-zA-Z0-9]+' > $RECENT_PASTES
+https_proxy='' curl https://pastebin.com/api_scraping.php | grep -E -o 'https://pastebin\.com/api_scrape_item\.php\?i=[a-zA-Z0-9]+' > $RECENT_PASTES
 echo "Recent pastes ($RECENT_PASTES):"
 cat $RECENT_PASTES
 cat $RECENT_PASTES | xargs -n 1 -P 10 -I {} /bin/sh -c "$SCRIPTDIR/extract_from_url.sh {} >> $ONION_URLS"
