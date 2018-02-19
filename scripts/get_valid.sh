f933183b238f7c0b7f1d6d9a06daa611a920f4e1
Felix Lehoux <flehoux@gosecure.ca>
2018-02-19 18:56:49 +0000

    Update the Reddit url to know the genuine and fake onions.

--- scripts/get_valid.sh
+++ scripts/get_valid.sh
@@ -7,7 +7,7 @@ LIST=`mktemp`
 echo "[+] Wiping current fake / genuine"
 echo $SQL | mysql -u $DB_USER  -h $DB_HOST --password=$DB_PASS $DB_BASE
 echo "[+] Getting superlist"
-$SCRIPTDIR/extract_from_url.sh https://www.reddit.com/r/darknetmarkets/wiki/superlist.json  > $LIST
+$SCRIPTDIR/extract_from_url.sh https://www.reddit.com/r/DNMSuperlist/wiki/superlist.json > $LIST
 echo "[?} VALID LIST:"
 cat $LIST
 echo "[+] Marking genuine ... "
