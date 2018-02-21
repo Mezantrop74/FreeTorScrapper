64b3c553f9c3322732cfd498b1a9e4851cd802eb
Félix Lehoux <32520954+MrL3X@users.noreply.github.com>
2018-02-21 15:30:44 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@ DIR=$( cd "$(dirname "$0")" ; pwd -P )
 . $DIR/env.sh
 (
 cd $BASEDIR/init
-echo "--- Shutdown services (haproxy, tor, delegate) ---"
+echo "--- Shutdown services ---"
 ./down_proxy.sh
 echo "--- Restart privoxy service ---"
 ./restart_privoxy.sh
@@ -11,9 +11,9 @@ echo "--- Start tor instance ---"
 ./start_tor.sh
 echo "\nTo check if all the links are working, test these command with a valid onion website. If you can see the page of your onion, you're all good."
 echo "curl --socks5-hostname 127.0.0.1:9051 http://darknt2vpswmwp5u.onion/"
-echo "curl --socks5-hostname 127.0.0.1:9058 http://darknt2vpswmwp5u.onion/"
+echo "curl --socks5-hostname 127.0.0.1:9054 http://darknt2vpswmwp5u.onion/"
 echo "curl --proxy 127.0.0.1:3129 http://darknt2vpswmwp5u.onion/"
-echo "curl --proxy 127.0.0.1:3126 http://darknt2vpswmwp5u.onion/"
+echo "curl --proxy 127.0.0.1:3132 http://darknt2vpswmwp5u.onion/"
 )
 
 
